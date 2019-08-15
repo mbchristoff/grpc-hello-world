2803c58fd949ee5e1f2a63ccebb5a170229c7abb
Nathan <nathanklumpenaar10@outlook.com>
2019-08-15 19:54:06 +0200

    greeter client

--- greeter_client.py
+++ greeter_client.py
@@ -4,20 +4,27 @@ from __future__ import print_function
 
 import grpc
 import sys
+import time
 
 import helloworld_pb2
 
 
 def run():
-  if len(sys.argv) >= 2:
-      grpc_server = sys.argv[1]
-  else:
-      grpc_srver = 'localhost:50051'
-  channel = grpc.insecure_channel(grpc_server)
-  stub = helloworld_pb2.GreeterStub(channel)
-  response = stub.SayHello(helloworld_pb2.HelloRequest(name='aaaaaaaaaaa'))
-  print("Greeter client received: " + response.message)
+    if len(sys.argv) >= 2:
+        grpc_server = sys.argv[1]
+    else:
+        grpc_server = 'localhost:50051'
+    channel = grpc.insecure_channel(grpc_server)
+    stub = helloworld_pb2.GreeterStub(channel)
+    response = stub.SayHello(helloworld_pb2.HelloRequest(name='aaaaaaaaaaa'))
+    print("Greeter client received: " + response.message)
 
 
 if __name__ == '__main__':
-  run()
+    while True:
+        try:
+            run()
+        except Exception as E:
+            print(E)
+        time.sleep(1)
+
